3826ad1244b830dbc8334e1afe608c6f4a0f9087
Genevieve Ferrari <genferrari@gmail.com>
2023-03-08 16:30:03 -0800

    rewrote code

--- script.js
+++ script.js
@@ -4,47 +4,123 @@ const questionBlock = document.getElementById("questionBlock");
 
 const question = document.getElementById("question");
 
-let answer1 = document.getElementById("a1");
-let answer2 = document.getElementById("a2");
-let answer3 = document.getElementById("a3");
+// let answer1 = document.getElementById("a1");
+// let answer2 = document.getElementById("a2");
+// let answer3 = document.getElementById("a3");
 
 const questions = [
   {
-    title: "What does an array store in JavaScript?",
-    choices: [
-      "a fixed-size collection of elements of the same data type",
-      "your hopes and dreams",
-      "only equality operators",
+    question: "What does an array store in JavaScript?",
+    answer: [
+      {
+        message: "a fixed-size collection of elements of the same data type",
+        right: true,
+      },
+      { message: "your hopes and dreams", right: false },
+      { message: "only equality operators", right: false },
     ],
-    answer: "a fixed-size collection of elements of the same data type",
   },
   {
-    title: "What function can be used to set a timer in Javascript?",
-    choices: [
-      "the setTimeout() method",
-      "the setInterval()method",
-      "both methods",
+    question: "What function can be used to set a timer in Javascript?",
+    answer: [
+      { message: "the setTimeout() method", right: false },
+      { message: "the setInterval()method", right: false },
+      { message: "both methods", right: true },
     ],
-    answer: "both methods",
   },
   {
-    title: "The querySelector() method allows you to:",
-    choices: [
-      "select the first element that matches one or more CSS selectors",
-      "run a function in the Dom",
-      "find your one true love",
+    question: "The querySelector() method allows you to:",
+    answer: [
+      {
+        message:
+          "select the first element that matches one or more CSS selectors",
+        right: true,
+      },
+      { message: "run a function in the Dom", right: false },
+      { message: "find your one true love", right: false },
     ],
-    answer: "both methods",
   },
   {
-    title: '"keyup", "keydown", and "keypress" are all examples of:',
-    choices: [
-      "Javasript elements",
-      "Javascript keyboard events",
-      "types of arrays",
+    question: '"keyup", "keydown", and "keypress" are all examples of:',
+    answer: [
+      { message: "Javasript elements", right: false },
+      { message: "Javascript keyboard events", right: true },
+      { message: "types of arrays", right: false },
     ],
-    answer: "Javascript keyboard events",
   },
 ];
 
+questions.forEach(
+  if answer.right === 
+)
+// const getRightAnswer = () => {
+//   questions.forEach(answer => {
+    
+//   });
+// }
+
+startButton.addEventListener("click", startQuiz);
+//whent start-button is clicked, timer starts and first question appears (how?)
+// startQuiz();
+
+function startQuiz() {
+  isWin = false;
+  timerCount = 10;
+  startTimer();
+}
+
+function startTimer() {
+  time = 60;
+  setTimeout(function () {
+    clocktick();
+  }, 1000);
+  function clocktick() {
+    time--;
+    if (time <= 0) {
+      quizEnd();
+    }
+    timerElement.textContent = time;
+  }
+}
+
 //https://stackoverflow.com/questions/72588081/working-on-a-javascript-quiz-app-and-having-an-issue-dynamically-generating-ques
+
+
+// const questions = [
+//   {
+//     title: "What does an array store in JavaScript?",
+//     choices: [
+//       "a fixed-size collection of elements of the same data type",
+//       "your hopes and dreams",
+//       "only equality operators",
+//     ],
+//     answer: "a fixed-size collection of elements of the same data type",
+//   },
+//   {
+//     title: "What function can be used to set a timer in Javascript?",
+//     choices: [
+//       "the setTimeout() method",
+//       "the setInterval()method",
+//       "both methods",
+//     ],
+//     answer: "both methods",
+//   },
+//   {
+//     title: "The querySelector() method allows you to:",
+//     choices: [
+//       "select the first element that matches one or more CSS selectors",
+//       "run a function in the Dom",
+//       "find your one true love",
+//     ],
+//     answer: "both methods",
+//   },
+//   {
+//     title: '"keyup", "keydown", and "keypress" are all examples of:',
+//     choices: [
+//       "Javasript elements",
+//       "Javascript keyboard events",
+//       "types of arrays",
+//     ],
+//     answer: "Javascript keyboard events",
+//   },
+// ];
